68d4fda7f9a90263994342819fa8846526d1b5a3
mahroonoohi <mahroonoohi@gmail.com>
2023-01-22 10:58:11 -0800

    added:

---
+++ Phaz2-2/RightSide.py
@@ -0,0 +1,26 @@
+
+from multiprocessing import  Process, Queue, Value,Array
+from time import sleep
+from Car import Car
+
+def producer(queue, id,):
+    print('Producer: Running', flush=True)
+    while True:
+        value = Car(id.value)
+        queue.put(value)
+        id.value += 1
+        sleep(value.time)
+
+
+def consumer(queue, street):
+    print('Consumer: Running', flush=True)
+    while True:
+        item = queue.get()     
+        street.value = item.id
+        print('car id: ', item.id, 'sleep: ', item.time)
+        temp = street.value
+        if temp != street.value:
+            print('Process conflict!')
+        sleep(item.time)
+   
+
